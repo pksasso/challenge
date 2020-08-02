ee55e9465652cc0d41b168c8d8fe70bb1e4ca364
Patrick <patricksassorj@gmail.com>
2020-08-02 15:38:40 -0300

    :sparkles: Add new colors

--- src/styles/Theme.js
+++ src/styles/Theme.js
@@ -9,4 +9,84 @@ export const theme = {
     dark: '#445E9C',
     light: '#9DB7F5',
   },
+  grass: {
+    primary: '#78C850',
+    dark: '#4E8234',
+    light: '#A7DB8D',
+  },
+  rock: {
+    primary: '#B8A038',
+    dark: '#786824',
+    light: '#D1C17D',
+  },
+  electric: {
+    primary: '#F8D030',
+    dark: '#A1871F',
+    light: '#FAE078',
+  },
+  poison: {
+    primary: '#A040A0',
+    dark: '#682A68',
+    light: '#C183C1',
+  },
+  psychic: {
+    primary: '#F85888',
+    dark: '#A13959',
+    light: '#FA92B2',
+  },
+  ground: {
+    primary: '#E0C068',
+    dark: '#927D44',
+    light: '#EBD69D',
+  },
+  bug: {
+    primary: '#A8B820',
+    dark: '#6D7815',
+    light: '#C6D16E',
+  },
+  dark: {
+    primary: '#705848',
+    dark: '#49392F',
+    light: '#A29288',
+  },
+  dragon: {
+    primary: '#7038F8',
+    dark: '#4924A1',
+    light: '#A27DFA',
+  },
+  fairy: {
+    primary: '#EE99AC',
+    dark: '#9B6470',
+    light: '#F4BDC9',
+  },
+  fighting: {
+    primary: '#C03028',
+    dark: '#7D1F1A',
+    light: '#D67873',
+  },
+  flying: {
+    primary: '#A890F0',
+    dark: '#6D5E9C',
+    light: '#C6B7F5',
+  },
+  ghost: {
+    primary: '#705898',
+    dark: '#493963',
+    light: '#A292BC',
+  },
+  ice: {
+    primary: '#98D8D8',
+    dark: '#638D8D',
+    light: '#BCE6E6',
+  },
+  normal: {
+    primary: '#A8A878',
+    dark: '#6D6D4E',
+    light: '#C6C6A7',
+  },
+  steel: {
+    primary: '#B8B8D0',
+    dark: '#787887',
+    light: '#D1D1E0',
+  },
 };
