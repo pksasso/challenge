5066ac375191477f74f54cd19cdbfbaefba85254
Patrick <patricksassorj@gmail.com>
2020-08-03 15:21:55 -0300

    :sparkles: Add context for all pokemon types

--- src/context/CartContext.js
+++ src/context/CartContext.js
@@ -4,72 +4,230 @@ export const CartContext = createContext();
 
 export const CartProvider = ({ children }) => {
   const getItemStorage = (storeName) => {
-    const localData = localStorage.getItem(storeName);
+    const localData = localStorage.getItem(`@${storeName}-store/cart`);
     return localData ? JSON.parse(localData) : [];
   };
 
-  const [fireCart, setFireCart] = useState(() => {
-    return getItemStorage('@fire-store/cart');
-  });
+  const setItemStorage = (storeName, cart) => {
+    localStorage.setItem(`@${storeName}-store/cart`, JSON.stringify(cart));
+  };
 
+  const [normalCart, setNormalCart] = useState(() => {
+    return getItemStorage('normal');
+  });
+  const [fightingCart, setFightingCart] = useState(() => {
+    return getItemStorage('fighting');
+  });
+  const [flyingCart, setFlyingCart] = useState(() => {
+    return getItemStorage('flying');
+  });
+  const [poisonCart, setPoisonCart] = useState(() => {
+    return getItemStorage('poison');
+  });
+  const [groundCart, setGroundCart] = useState(() => {
+    return getItemStorage('ground');
+  });
+  const [rockCart, setRockCart] = useState(() => {
+    return getItemStorage('rock');
+  });
+  const [bugCart, setBugCart] = useState(() => {
+    return getItemStorage('bug');
+  });
+  const [ghostCart, setGhostCart] = useState(() => {
+    return getItemStorage('ghost');
+  });
+  const [steelCart, setSteelCart] = useState(() => {
+    return getItemStorage('steel');
+  });
+  const [fireCart, setFireCart] = useState(() => {
+    return getItemStorage('fire');
+  });
   const [waterCart, setWaterCart] = useState(() => {
-    return getItemStorage('@water-store/cart');
+    return getItemStorage('water');
+  });
+  const [grassCart, setGrassCart] = useState(() => {
+    return getItemStorage('grass');
+  });
+  const [electricCart, setElectricCart] = useState(() => {
+    return getItemStorage('electric');
+  });
+  const [psychicCart, setPsychicCart] = useState(() => {
+    return getItemStorage('psychic');
+  });
+  const [iceCart, setIceCart] = useState(() => {
+    return getItemStorage('ice');
+  });
+  const [dragonCart, setDragonCart] = useState(() => {
+    return getItemStorage('dragon');
+  });
+  const [darkCart, setDarkCart] = useState(() => {
+    return getItemStorage('dark');
+  });
+  const [fairyCart, setFairyCart] = useState(() => {
+    return getItemStorage('fairy');
   });
 
   useEffect(() => {
-    localStorage.setItem('@fire-store/cart', JSON.stringify(fireCart));
-    return () => {};
+    setItemStorage('normal', normalCart);
+  }, [normalCart]);
+
+  useEffect(() => {
+    setItemStorage('fighting', fightingCart);
+  }, [fightingCart]);
+
+  useEffect(() => {
+    setItemStorage('flying', flyingCart);
+  }, [flyingCart]);
+
+  useEffect(() => {
+    setItemStorage('poison', poisonCart);
+  }, [poisonCart]);
+
+  useEffect(() => {
+    setItemStorage('ground', groundCart);
+  }, [groundCart]);
+
+  useEffect(() => {
+    setItemStorage('rock', rockCart);
+  }, [rockCart]);
+
+  useEffect(() => {
+    setItemStorage('bug', bugCart);
+  }, [bugCart]);
+
+  useEffect(() => {
+    setItemStorage('ghost', ghostCart);
+  }, [ghostCart]);
+
+  useEffect(() => {
+    setItemStorage('steel', steelCart);
+  }, [steelCart]);
+
+  useEffect(() => {
+    setItemStorage('fire', fireCart);
   }, [fireCart]);
 
   useEffect(() => {
-    localStorage.setItem('@water-store/cart', JSON.stringify(waterCart));
-    return () => {};
+    setItemStorage('water', waterCart);
   }, [waterCart]);
 
+  useEffect(() => {
+    setItemStorage('grass', grassCart);
+  }, [grassCart]);
+
+  useEffect(() => {
+    setItemStorage('electric', electricCart);
+  }, [electricCart]);
+
+  useEffect(() => {
+    setItemStorage('psychic', psychicCart);
+  }, [psychicCart]);
+
+  useEffect(() => {
+    setItemStorage('ice', iceCart);
+  }, [iceCart]);
+
+  useEffect(() => {
+    setItemStorage('dragon', dragonCart);
+  }, [dragonCart]);
+
+  useEffect(() => {
+    setItemStorage('dark', darkCart);
+  }, [darkCart]);
+
+  useEffect(() => {
+    setItemStorage('fairy', fairyCart);
+  }, [fairyCart]);
+
+  const cartsType = {
+    normal: {
+      cart: normalCart,
+      setCart: setNormalCart,
+    },
+    fighting: {
+      cart: fightingCart,
+      setCart: setFightingCart,
+    },
+    flying: {
+      cart: flyingCart,
+      setCart: setFlyingCart,
+    },
+    poison: {
+      cart: poisonCart,
+      setCart: setPoisonCart,
+    },
+    ground: {
+      cart: groundCart,
+      setCart: setGroundCart,
+    },
+    rock: {
+      cart: rockCart,
+      setCart: setRockCart,
+    },
+    bug: {
+      cart: bugCart,
+      setCart: setBugCart,
+    },
+    ghost: {
+      cart: ghostCart,
+      setCart: setGhostCart,
+    },
+    ghost: {
+      cart: ghostCart,
+      setCart: setGhostCart,
+    },
+    steel: {
+      cart: fireCart,
+      setCart: setFireCart,
+    },
+    water: {
+      cart: waterCart,
+      setCart: setWaterCart,
+    },
+    grass: {
+      cart: grassCart,
+      setCart: setGrassCart,
+    },
+    electric: {
+      cart: electricCart,
+      setCart: setElectricCart,
+    },
+    psychic: {
+      cart: psychicCart,
+      setCart: setPsychicCart,
+    },
+    ice: {
+      cart: iceCart,
+      setCart: setIceCart,
+    },
+    dragon: {
+      cart: dragonCart,
+      setCart: setDragonCart,
+    },
+    dark: {
+      cart: darkCart,
+      setCart: setDarkCart,
+    },
+    fairy: {
+      cart: fairyCart,
+      setCart: setFairyCart,
+    },
+  };
+
   const getCartByType = (type) => {
-    switch (type) {
-      case 'fire':
-        return fireCart;
-      case 'water':
-        return waterCart;
-      default:
-        return 0;
-    }
+    return cartsType[type].cart;
   };
 
   const addCartByType = (type, pokemon) => {
-    switch (type) {
-      case 'fire':
-        addCart(pokemon, fireCart, setFireCart);
-        break;
-      case 'water':
-        addCart(pokemon, waterCart, setWaterCart);
-        break;
-      default:
-        return 0;
-    }
+    addCart(pokemon, cartsType[type].cart, cartsType[type].setCart);
   };
 
   const getTotalPriceByType = (type) => {
-    switch (type) {
-      case 'fire':
-        return totalPrice(fireCart);
-      case 'water':
-        return totalPrice(waterCart);
-      default:
-        return 0;
-    }
+    return totalPrice(cartsType[type].cart);
   };
 
   const clearCartByType = (type) => {
-    switch (type) {
-      case 'fire':
-        return clearCart(fireCart, setFireCart);
-      case 'water':
-        return clearCart(waterCart, setWaterCart);
-      default:
-        return 0;
-    }
+    return clearCart(cartsType[type].cart, cartsType[type].setCart);
   };
 
   const totalPrice = (cart) => {
